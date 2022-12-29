a88e0561cd828dcce9a1e55fe8496d0b2090aeca
Noer Paanakker <noer@lightworksweb.nl>
2022-12-29 22:40:19 +0100

    styled individual posts

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -17,8 +17,11 @@ export default function PostPage({ params }: TPostPageProps) {
   const post = getPostContent(params.slug);
   return (
     <main>
-      <h1>{post.data.title}</h1>
-      <Markdown>{post.content}</Markdown>
+      <h6 className="text-md text-slate-400">{post.data.date}</h6>
+      <h1 className="font-bold text-3xl text-violet-600">{post.data.title}</h1>
+      <article className="prose lg:prose-xl">
+        <Markdown>{post.content}</Markdown>
+      </article>
     </main>
   );
 }
